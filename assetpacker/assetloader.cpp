4bfec936534d6067abdecde70eb635f9f59c3ade
Imreness <berimr02@gamil.com>
2021-06-15 10:53:35 +0200

    3dassetpacking works

--- assetpacker/assetloader.cpp
+++ assetpacker/assetloader.cpp
@@ -21,12 +21,12 @@
     width*height*4*8bit - data
 
     per model:
+    25 * char - model name
     8bit int - textureID
     32bit int - verticesamount
-    Xbit - vertices
+    float_t  - vertices
     32bit int - indicies amount
-    Xbit - indicies
-
+    int32 - indicies
 
 */
 
@@ -34,7 +34,7 @@ namespace assetloader{
     uint8_t textureAmount = 0;
     uint8_t modelAmount = 0;
 
-    void packmodel(const char* modelpath, std::ofstream& file);
+    void packmodel(const char* modelpath, binaryFile& file);
 
     void pack(){
         std::printf("Name a subfolder\n");
@@ -47,64 +47,99 @@ namespace assetloader{
         std::string outfilePath("../assets/");
         outfilePath.append(subfolder.c_str()).append(".assetbundle");
 
-        std::ofstream outfile;
-        outfile.open(outfilePath.c_str(), std::ios::out | std::ios::binary);
+        binaryFile outfile;
+        outfile.open(outfilePath.c_str(), true);
 
         const char* placeholder = new char[1];
         //place reserved for texture amount
-        outfile.write(placeholder, sizeof(char));
+        outfile.writeChars(placeholder, 1);
         //place reserved for model amount
-        outfile.write(placeholder, sizeof(char));
+        outfile.writeChars(placeholder, 1);
 
 
         textureAmount = 0;
         modelAmount = 0;
 
-        while(true){
-            std::printf("Name a file to pack\n");
-             
-            std::string assetfileName;
-            std::getline(std::cin, assetfileName);
-            std::string modelpath("assets/");
-            modelpath.append(subfolder.c_str()).append("/").append(assetfileName);
-            packmodel(modelpath.c_str(), outfile);
-        }
+        std::printf("Name a file to pack\n");
+            
+        std::string assetfileName;
+        std::getline(std::cin, assetfileName);
+        std::string modelpath("assets/");
+        modelpath.append(subfolder.c_str()).append("/").append(assetfileName);
+        packmodel(modelpath.c_str(), outfile);
 
-        outfile.seekp(0);
-        outfile.write((char*)(&modelAmount), sizeof(uint8_t));
+        outfile.write(textureAmount, 0);
+        outfile.write(modelAmount, 1);
 
         outfile.close();
     }
 
-    void packtextures(const aiScene* level, std::ofstream& file){
+    void packtextures(const aiScene* level, binaryFile& file, std::string texturepath){
         textureAmount = level->mNumMaterials;
         for(int i = 0; i < textureAmount; i++){
             aiString texPath;
             level->mMaterials[i]->GetTexture(aiTextureType::aiTextureType_DIFFUSE, 0,&texPath);
 
+            std::string realTexturePath(texturepath);
+            realTexturePath.append(texPath.C_Str());
+
             int textureX, textureY, channels;
             stbi_set_flip_vertically_on_load(true);
-            unsigned char* data = stbi_load(texPath.C_Str(), &(textureX), &(textureY), &channels, 4);
+            unsigned char* data = stbi_load(realTexturePath.c_str(), &(textureX), &(textureY), &channels, 4);
 
-            file.write((char*)&textureX, sizeof(uint16_t));
-            file.write((char*)&textureY, sizeof(uint16_t));
-            file.write((char*)data , textureX*textureY*4);
+            file.write((uint16_t)textureX);
+            file.write((uint16_t)textureY);
+            file.writeChars(data, textureX*textureY*4);
         }
     }
 
-    void packmodel(const char* modelpath, std::ofstream& file){
+    void packmodel(const char* modelpath, binaryFile& file){
         Assimp::Importer importer;
         const aiScene* level = importer.ReadFile(modelpath, aiProcess_OptimizeMeshes);
 
-        packtextures(level, file);
+        std::string modelpath_string(modelpath);
+        std::string modelbasedir = modelpath_string.substr(0, modelpath_string.find_last_of('/') + 1);
 
-        // aiNode rootNode = level->mRootNode;
+        packtextures(level, file, modelbasedir);
 
-        // for(int i = 0; i < rootNode.mNumChildren; i++){
-        //     aiNode currNode = rootNode.mChildren[i];
+        aiNode* rootNode = level->mRootNode;
 
+        for(int i = 0; i < rootNode->mNumChildren; i++){
+            modelAmount++;
 
-        // }
+            aiNode* currNode = rootNode->mChildren[i];
+
+            aiMesh* nodeMesh = level->mMeshes[currNode->mMeshes[0]];
+
+            aiString name = currNode->mName;
+            
+            file.writeChars(name.C_Str(), 25);
+
+            name.Clear();
+
+            file.write((uint8_t)nodeMesh->mMaterialIndex);
+
+            //multiply numvertices with 5, since for each vertices, we got 5 data
+            // 3 poisiton and 2 texture coords
+            file.write((uint32_t)(nodeMesh->mNumVertices));
+
+            for(int i = 0; i < nodeMesh->mNumVertices; i++){
+                file.write((double_t)nodeMesh->mVertices[i].x);
+                file.write((double_t)nodeMesh->mVertices[i].y);
+                file.write((double_t)nodeMesh->mVertices[i].z);
+
+                file.write((double_t)nodeMesh->mTextureCoords[0][i].x);
+                file.write(-(double_t)(nodeMesh->mTextureCoords[0][i].y));
+            }
+
+            file.write((uint32_t)(nodeMesh->mNumFaces));
+
+            for(int i = 0; i < nodeMesh->mNumFaces; i++){
+                file.write((uint32_t)nodeMesh->mFaces[i].mIndices[0]);
+                file.write((uint32_t)nodeMesh->mFaces[i].mIndices[1]);
+                file.write((uint32_t)nodeMesh->mFaces[i].mIndices[2]);
+            }
+        }
     }
 
 }
