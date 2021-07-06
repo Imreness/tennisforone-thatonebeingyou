df6c9b9513653717d35cb6c2656c59547377a02f
Imre Barta <berimr02@gmail.com>
2021-07-06 17:53:01 +0200

    added nvidia support

--- code/core/game.cpp
+++ code/core/game.cpp
@@ -31,7 +31,7 @@ void Game::initGLFW(const char* title){
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     //Init window here
